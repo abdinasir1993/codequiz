8e1e3887ce6d6880bc2e67603cf69b2c4dca4d3f
abdinasir1993 <abdinasir_jirde@hotmail.com>
2022-05-05 15:04:42 +0100

    start button function added to javascript

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+// target start button
+const startButton = document.getElementById("start-btn")
+
+// target banner section
+const bannerSection = document.getElementById("banner")
+
+//declare the event handler function for start button click
+const handleStartButtonClick = () => {
+console.log("start button clicked") };
+// add event listener to start button
+startButton.addEventListener("click", handleStartButtonClick )
