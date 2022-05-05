41e29daf4569d4c994eb68925821168d91914e61
abdinasir1993 <abdinasir_jirde@hotmail.com>
2022-05-05 17:18:05 +0100

    included quiz questions

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,36 @@
 // target start button
-const startButton = document.getElementById("start-btn")
+const startButton = document.getElementById("start-btn");
 
 // target banner section
-const bannerSection = document.getElementById("banner")
+const bannerSection = document.getElementById("banner");
+
+
+
+// function to render question to page
+const renderQuestion = () => {
+    console.log("render question");
+    
+}
+
+//remove banner section
+const removeBanner = () => {
+    console.log ("remove banner");
+    
+bannerSection.remove();
+   
+};
 
 //declare the event handler function for start button click
 const handleStartButtonClick = () => {
-console.log("start button clicked") };
+
+    console.log("start button clicked");
+
+
+removeBanner();
+renderQuestion();
+
+}
 // add event listener to start button
-startButton.addEventListener("click", handleStartButtonClick )
+startButton.addEventListener("click", handleStartButtonClick);
+
+// remove 
